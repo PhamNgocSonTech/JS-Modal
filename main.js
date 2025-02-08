b2eed10a06085ddc9afce0d3bc4e38d40526f7dd
PhamNgocSonTech <phamngocson7a1@gmail.com>
2025-02-08 18:20:03 +0700

    add: close modal with Esc key

--- main.js
+++ main.js
@@ -4,16 +4,22 @@ const $$ = document.querySelectorAll.bind(document);
 const modal = $("#modal");
 
 $("#open-modal").onclick = function () {
-  $("#modal").classList.add("show");
+  modal.classList.add("show");
 };
-// console.log($("#modal"));
+// console.log(modal);
 
 $("#modal-close").onclick = function () {
-  $("#modal").classList.remove("show");
+  modal.classList.remove("show");
 };
 
-$("#modal").onclick = function (e) {
-  if (e.target === $("#modal")) {
-    $("#modal").classList.remove("show");
+modal.onclick = function (e) {
+  if (e.target === modal) {
+    modal.classList.remove("show");
   }
 };
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape") {
+    modal.classList.remove("show");
+  }
+});
